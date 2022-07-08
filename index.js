c4c11af0161e7cf1af28814b92f4d0470d7970b7
KCampbell22 <ksoup12@gmail.com>
2022-07-07 22:45:08 -0500

    mostly finished

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 //Navbar
+
+
 document.querySelector('.hamburger').addEventListener('click', function () {
     document.querySelector('.nav-bar').classList.toggle('open');
+    document.body.style.backgroundColor = '#C7DFCA';
+    document.querySelector('.line-one').style.backgroundColor = '#3D3F5D';
+    document.querySelector('.line-two').style.backgroundColor = '#3D3F5D';
+    document.querySelector('.line-three').style.backgroundColor = '#3D3F5D';
+    if (document.querySelector('.nav-bar').classList.contains('open')) {
+        document.body.style.backgroundColor = '#C7DFCA';
+    } else {
+        document.body.style.backgroundColor = '#fff';
+    }
 });
+
 
